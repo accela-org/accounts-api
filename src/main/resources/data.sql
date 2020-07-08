a672534cc0df5ab038fa55e7d54f65d9c966c846
Andrej Lavrinovic <andrei.lavrinovic@gmail.com>
2020-07-08 12:44:15 +0100

    fix primary key conflict

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,1 @@
-insert into user values(1, 'Andrej', 'Lavrinovic');
-insert into user values(2, 'John', 'White');
-insert into user values(3, 'Conor', 'McGregor');
-insert into user values(4, 'Conan', 'Doyle');
+insert into user values(0, 'Andrej', 'Lavrinovic');
